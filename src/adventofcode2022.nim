7b4ffd1d8d1afc2c7b701175be3d37386488c5f6
mishankov <denis@mishankov.dev>
2022-12-07 21:52:13 +0300

    day 7

--- src/adventofcode2022.nim
+++ src/adventofcode2022.nim
@@ -1,5 +1,5 @@
 import day01_1, day01_2, day02_1, day02_2, day03_1, day03_2, day04_1, day04_2,
-    day05_1, day05_2, day06_1, day06_2
+    day05_1, day05_2, day06_1, day06_2, day07_1, day07_2
 
 if isMainModule:
   day01_1.solve()
@@ -14,3 +14,5 @@ if isMainModule:
   day05_2.solve()
   day06_1.solve()
   day06_2.solve()
+  day07_1.solve()
+  day07_2.solve()
---
+++ src/day07_1.nim
@@ -0,0 +1,84 @@
+import streams, strutils, strscans
+
+type
+  File = object
+    name: string
+    size: int
+
+  Folder = object
+    name: string
+    folders: seq[Folder]
+    files: seq[File]
+    size: int
+
+  LineType = enum CommandCd, CommandLs, OutputFile, OutputFolder
+
+
+proc addFile(folder: var Folder, file: File, directory: seq[string]) =
+  if directory.len() == 1:
+    folder.files.add(file)
+    return
+
+  for i in countup(0, folder.folders.len() - 1):
+    if folder.folders[i].name == directory[1]:
+      folder.folders[i].addFile(file, directory[1..^1])
+      return
+
+  folder.folders.add(Folder(name: directory[1]))
+  folder.folders[^1].addFile(file, directory[1..^1])
+
+proc calculateSize(rootFolder: Folder): int =
+  for file in rootFolder.files:
+    result += file.size
+
+  for folder in rootFolder.folders:
+    result += folder.calculateSize()
+
+proc folderSizes(rootFolder: Folder, list: var seq[Folder]) =
+  list.add(Folder(name: rootFolder.name, size: rootFolder.calculateSize()))
+  for folder in rootFolder.folders: folder.folderSizes(list)
+
+
+proc getLineType(line: string): LineType =
+  if line.startsWith("$ cd"): return CommandCd
+  if line.startsWith("$ ls"): return CommandLs
+  if line.startsWith("dir"): return OutputFolder
+  return OutputFile
+
+proc solve*() =
+  let data = newFileStream("data/day07.txt")
+  defer: data.close()
+
+  var pwd = @["/"]
+  var fileSystem = Folder(name: "/")
+  while not data.atEnd():
+    let line = data.readLine()
+
+    case line.getLineType():
+      of CommandCd:
+        let destination = line[5..^1]
+        if destination == "/": pwd = @["/"]
+        elif destination == "..": pwd = pwd[0..^2]
+        else: pwd.add(destination)
+
+      of OutputFile:
+        var fileName: string
+        var fileSize: int
+        if scanf(line, "$i $w", fileSize, fileName):
+          fileSystem.addFile(File(name: fileName, size: fileSize), pwd)
+
+      of CommandLs: discard
+      of OutputFolder: discard
+
+  var foldersList: seq[Folder]
+  fileSystem.folderSizes(foldersList)
+  var totalSize = 0
+
+  for folder in foldersList:
+    if folder.size <= 100000:
+      totalSize += folder.size
+
+  echo "7.1 Answer: ", totalSize
+
+if isMainModule:
+  solve()
